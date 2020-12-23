ff832582a2863caeee4b233c7a497e9542f39409
kurozemi <tranngminhdao@gmail.com>
2020-12-23 16:05:08 +0700

    Revert "<3"
    This reverts commit e9d5029a3eac1561573d842682491c3dd9995c12.

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -34,6 +34,8 @@ const SignUp = ({ navigation }) => {
     const [phone, setPhone] = useState('');
     const [isMale, setIsMale] = useState(true);
     const [isFemale, setIsFemale] = useState(false);
+    const [isOtherGender, setIsOtherGender] = useState(false);
+
 
     const checkIsBlank = () => {
         if (username == '' || password == '' || email == '' || phone == '')
@@ -41,6 +43,9 @@ const SignUp = ({ navigation }) => {
         return false;
     }
 
+    const selectGender = (gender) => {
+
+    }
 
     return (
         <View style={signup_style.main}>
